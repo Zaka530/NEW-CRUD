ca1a31d5f6395bcef196ac2afe5ba5255cb0dc12
Zaka530 <kbariev7@gmail.com>
2024-04-03 06:36:54 +0500

    lesson 20

--- src/main/java/uz/kamron/springcourse/controllers/FirstController.java
+++ src/main/java/uz/kamron/springcourse/controllers/FirstController.java
@@ -2,6 +2,7 @@ package uz.kamron.springcourse.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,9 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false)String name,
-                            @RequestParam(value = "surname",required = false)String surname){
+                            @RequestParam(value = "surname",required = false)String surname,
+                            Model model){
+        model.addAttribute("message","Hello, "+name+" "+surname);
 
         System.out.println("Hello, "+name+" "+surname);
 
@@ -27,5 +30,33 @@ public class FirstController {
     public String goodByePage(){
         return "first/goodbye";
 
+    }
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b")int b,
+                             @RequestParam("action")String action,
+                             Model model){
+        double result;
+
+        switch (action){
+            case "multiplication":
+                result=a*b;
+                break;
+            case "devision":
+                result=a/(double)b;
+                break;
+            case "subtraction":
+                result=a-b;
+                break;
+            case "addition":
+                result=a+b;
+                break;
+            default:
+                result=0;
+                break;
+        }
+
+
+        model.addAttribute("result",result);
+        return "first/calculator";
     }
 }
