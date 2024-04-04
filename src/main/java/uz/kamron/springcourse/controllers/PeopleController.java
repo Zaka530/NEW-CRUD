f7617a47e38cbb4c73b0aba6fb766006661f6e35
Zaka530 <kbariev7@gmail.com>
2024-04-04 14:07:19 +0500

    CRUD V2

--- src/main/java/uz/kamron/springcourse/controllers/PeopleController.java
+++ src/main/java/uz/kamron/springcourse/controllers/PeopleController.java
@@ -3,35 +3,48 @@ package uz.kamron.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import uz.kamron.springcourse.dao.PersonDAO;
+import uz.kamron.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-private final PersonDAO personDAO;
+    private final PersonDAO personDAO;
 
-@Autowired
+    @Autowired
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
 
     @GetMapping()
-    public String index(Model model){
+    public String index(Model model) {
 
-    model.addAttribute("people",personDAO.index());
+        model.addAttribute("people", personDAO.index());
         //получим всех людей из DAO и передадим на обображение в представление
-        return "people/index";}
+        return "people/index";
+    }
+
     @GetMapping("/{id}")
-public String show(@PathVariable("id")int id,Model model){
+    public String show(@PathVariable("id") int id, Model model) {
         //Получим одного человека по id из DAO и передадим на отображение в представление
-        model.addAttribute("person",personDAO.show(id));
-return "people/show";
+        model.addAttribute("person", personDAO.show(id));
+
+        return "people/show";
+    }
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person",new Person());
+
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person")Person person){
+        personDAO.save(person);
+
+        return "redirect:/people";
+    }
 }
 
-
-}
